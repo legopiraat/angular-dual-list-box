cbb1fa19e931d3228ea26faba78d3b42936e02cd
Gijs van der Venne <Gijs.vandervenne@infosupport.com>
2016-03-03 14:40:05 +0100

    Moved buttons to the middle and changed test data

--- README.md
+++ README.md
@@ -9,5 +9,5 @@ This is a simple angular directive to make a dual-list-box.
 HTML tag:
 `<dual-list-box options-left="left" options-right="right"></dual-list-box>`
 
-options-left will be the content of the list-box on the left.
+options-left will be the content of the list-box on the left. 
 options-right will be the content of the list-box on the right.
--- app/scripts/mainController.js
+++ app/scripts/mainController.js
@@ -3,6 +3,6 @@
 angular.module('dualListBox')
   .controller('mainController', function ($scope) {
 
-    $scope.left = ['België', 'BUO', 'BUZ', 'Data Solutions', 'De Vries', 'ISRES', 'Kennis Centrum'];
-    $scope.right = ['BUF', 'BUI'];
+    $scope.left = ['Option 1', 'Option 2', 'Option 3', 'Option 4', 'Option 5', 'Option 6'];
+    $scope.right = ['Option 7', 'Option 8', 'Option 9', 'Option 10'];
   });
